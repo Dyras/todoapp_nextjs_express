153b6b2dc52e51fb5c5ccee26afd8eebd08b2218
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-06-16 18:46:35 +0200

    Add a PUT for editing a TODO + future plans

--- apps/expressbackend/src/main.ts
+++ apps/expressbackend/src/main.ts
@@ -2,6 +2,10 @@ import { Todo } from './Itodo';
 import express from 'express';
 import fs from 'fs';
 
+// Things to do:
+// 1. Move to using in-memory storage
+// 2. Add a PUT for completing a todo
+
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
@@ -96,6 +100,49 @@ app.get('/api/todos/:id', (req, res) => {
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: 'Failed to read todo' });
+    }
+  });
+});
+
+// Edit a todo
+app.put('/api/todos', (req, res) => {
+  console.log(req.body);
+  const { id, title, userId, description } = req.body;
+  const fileName = `${userId}.json`;
+
+  fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        return res.status(404).json({ error: 'Todo not found' });
+      }
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read todo' });
+    }
+
+    try {
+      const todos = JSON.parse(data);
+      const todoToUpdate = todos.todos.find((todo) => todo.id === id);
+
+      if (!todoToUpdate) {
+        return res.status(404).json({ error: 'Todo not found' });
+      }
+
+      // Update the fields of the todo
+      todoToUpdate.title = title || todoToUpdate.title;
+      todoToUpdate.description = description || todoToUpdate.description;
+      todoToUpdate.status = req.body.status || todoToUpdate.status;
+
+      fs.writeFile(fileName, JSON.stringify(todos, null, 2), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Failed to update todo' });
+        }
+        console.log('Todo updated:', todoToUpdate);
+        return res.status(200).json({ message: 'Todo updated successfully' });
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Failed to update todo' });
     }
   });
 });
