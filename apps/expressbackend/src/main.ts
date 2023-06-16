3a2691ee57b1eaf0493cdf7f55208475204d4db4
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-06-16 16:24:19 +0200

    Add post API for sending in todos

--- apps/expressbackend/src/main.ts
+++ apps/expressbackend/src/main.ts
@@ -1,14 +1,32 @@
+import { Todo } from './Itodo';
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+app.use(express.json());
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
+});
+
+app.post('/api/todos', (req, res) => {
+  const { id, title, userId, description } = req.body;
+  const status = false;
+  if (!id) {
+    return res.status(400).json({ error: 'Invalid todo data, id is missing' });
+  } else if (!userId) {
+    return res
+      .status(400)
+      .json({ error: 'Invalid todo data, userId is missing' });
+  }
+
+  const newTodo: Todo = { id, title, userId, description, status };
+
+  console.log('New todo created:', newTodo);
+
+  return res
+    .status(201)
+    .json({ message: 'Todo created successfully', todo: newTodo });
 });
